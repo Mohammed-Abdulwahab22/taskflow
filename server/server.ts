5afc9b53c18040cd485a42d1bcb1c5894d0b5b24
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-06-25 15:36:14 +0300

    setting up structure in prisma client and server.ts + installing cors.

---
+++ server/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import authRoutes from './routes/authRoutes';
+import taskRoutes from './routes/taskRoutes';
+import { verifyToken } from "./middleware/auth";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', verifyToken, taskRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
