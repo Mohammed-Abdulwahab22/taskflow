42751f521a3e9dbf97fd72fcb26b55dcdcdd2e27
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-07-03 12:54:14 +0300

    adding logout button + adjusting sidebar header style.

--- client/src/components/DashboardSidebar.tsx
+++ client/src/components/DashboardSidebar.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import '../styles/DashboardSidebar.css';
 
 export const DashboardSidebar = () => {
+  const email = localStorage.getItem("Email") || "No email found";
+
   return (
     <div className='dashboard-sidebar'>
         
         <div className='dashboard-sidebar-header'>
-          <h3 className=''>logged in email</h3>
+          <h3 className='logged-in-email'>{email}</h3>
+          <button className='logout-button' >Logout</button>
         </div>
 
     </div>
