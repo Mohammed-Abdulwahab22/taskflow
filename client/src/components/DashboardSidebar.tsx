0d256fd00634d93cde0468951f438bc02e78b384
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-07-03 13:19:23 +0300

    adding modal logic in dashboard sidebar.

--- client/src/components/DashboardSidebar.tsx
+++ client/src/components/DashboardSidebar.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import Modal from 'react-modal';
 
 import '../styles/DashboardSidebar.css';
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('#root');
+
 export const DashboardSidebar = () => {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   const email = localStorage.getItem("email") || "No email found";
   const navigate = useNavigate();
 
@@ -23,8 +46,25 @@ export const DashboardSidebar = () => {
         <button className='logout-button' onClick={() => (handleLogout())}>Logout</button>
       </div>
 
-      <div className='add-task-form-button' >
-        
+      <div className='add-task-form-button' onClick={openModal}>
+        <span className='add-task-icon'>+</span>
+        <span className='add-task-text'>Add Task</span>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <button onClick={closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
       </div>
 
     </div>
